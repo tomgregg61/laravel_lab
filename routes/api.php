414382930c280119fc73d7254410e8e2902ceb76
Tom Gregg <thomasgregg05@outlook.com>
2025-03-31 11:12:44 +0100

    add chirp api routes

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Resources\ChirpResource;
+use App\Models\Chirp;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,4 +18,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/chirp/{chirpid}', function (string $id) {
+    return new ChirpResource(Chirp::with('user')->findOrFail($id));
+});
+
+Route::get('/chirps', function () {
+    return ChirpResource::collection(Chirp::all());
 });
