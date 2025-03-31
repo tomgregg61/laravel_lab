191482686e0824fbdff2123fedadef7b9155b01d
Tom Gregg <thomasgregg05@outlook.com>
2025-03-31 11:12:15 +0100

    add favourites route

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,9 @@
 <?php
 
+use App\Http\Controllers\ChirpController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
@@ -22,10 +13,14 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::middleware('auth')->group(function () {
+Route::middleware('auth', 'verified')->group(function () {
+    Route::resource('chirps', ChirpController::class)
+        ->only(['index', 'store', 'edit', 'update', 'destroy']);
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::post('/chirps/{chirp}/addToFavourites', [ChirpController::class, 'addToFavourites'])->name('chirps.favourites.add');
+    Route::get('/chirps/favourites', [ChirpController::class, 'favourites'])->name('chirps.favourites');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
